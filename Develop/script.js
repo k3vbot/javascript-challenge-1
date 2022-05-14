254714ea0b0c63aa5f36d97cd906fe2c5ac5d8e4
Kevin <muehl004@gmail.com>
2022-05-13 22:16:10 -0700

    added user prompt

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var pswdLength = prompt("Please enter desired password length (8-128 Characters)");
+    alert (pswdLength);
+  if (pswdLength <= 7){
+    alert("Too Weak! Please enter a number between 8 and 128");
+  }
+  else if (pswdCaps)
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+ 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
