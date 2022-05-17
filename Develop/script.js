239824e8e59be2e123f60a0105e37727c81a2487
Kevin <muehl004@gmail.com>
2022-05-16 18:23:39 -0700

    added key string

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const key_strings = {
+  lowercase : 'abcdefghijklmnopqrstuvwxyz',
+  uppercase : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  number : '1234567890',
+  specialchar : '!@#$%^&*',
+}
+
 // Write password to the #password input
 function writePassword() {
   var pswdLength = prompt("Please enter desired password length (8-128 Characters)");
-    alert (pswdLength);
   if (pswdLength <= 7){
     alert("Too Weak! Please enter a number between 8 and 128");
   }
-  else if (pswdCaps)
+  else if (pswdLength > 128){
+    alert("Too Long! Please enter a number between 8 and 128") 
+  };
+
+  var lowerCase = confirm("Should the password contain lowercase characters?");
+  var upperCase = confirm("Should the password contain upper case characters?");
+  var numberChar = confirm("Should the password contain numbers?")
+  var specialChar = confirm("Should the password contain special characters?");
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
